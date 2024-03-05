8c047784c90706cd2b0e7e0bee945e52e9237f15
SamStar3 <samstar1044948@gmail.com>
2024-03-05 22:16:21 +0530

    file added

---
+++ Recursions/src/DSA/hanoi.java
@@ -0,0 +1,24 @@
+package DSA;
+
+public class hanoi {
+
+	public static void toh(int n, char a, char b, char c) {
+
+		if (n == 1) {
+			System.out.println("Move 1 from " + a + " to " + c);
+			return;
+		}
+		toh(n - 1, a, c, b);
+		System.out.println("Move " + n + " from " + a + " to " + c);
+		toh(n - 1, b, a, c);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		toh(2, 'a', 'b', 'c');
+
+	}
+
+}
