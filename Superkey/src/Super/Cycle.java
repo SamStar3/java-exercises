f351eb72928639ff0aa94fd2b06962c56b139149
SamStar3 <samstar1044948@gmail.com>
2024-03-12 22:05:41 +0530

    file added

---
+++ Superkey/src/Super/Cycle.java
@@ -0,0 +1,33 @@
+package Super;
+
+class BiCycle {
+	String define_me() {
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle {
+	String define_me() {
+		return "a cycle with an engine.";
+	}
+
+	MotorCycle() {
+		System.out.println("Hello I am a motorcycle, I am " + define_me());
+
+		String temp = super.define_me(); // Fix this line
+
+		System.out.println("My ancestor is a cycle who is " + temp);
+	}
+
+}
+
+public class Cycle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MotorCycle M = new MotorCycle();
+
+	}
+
+}
